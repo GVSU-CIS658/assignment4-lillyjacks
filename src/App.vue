e74938a65aab915e25af3b6260c715372c08f09a
Jasoline <lillybird365@gmail.com>
2026-03-26 13:51:22 -0400

    pinia store integration

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
+<! -- import {createApp} from "vue";
+import App from "./App.vue";
+import {createPinia} from "pinia";
+
+createApp(App)
+  .use(createPinia())
+  .mount("#app"); -->
+
 <template>
   <div>
     <Beverage
--- src/stores/beverageStore.ts
+++ src/stores/beverageStore.ts
@@ -1,15 +1,58 @@
 import { defineStore } from "pinia";
 import tempretures from "../data/tempretures.json";
+import syrups from "../data/syrups.json";
+import creamers from "../data/creamers.json";
+import bases from "../data/bases.json";
+import { BaseBeverageType, CreamerType, SyrupType, BeverageType } from "../types/beverage";
 
 export const useBeverageStore = defineStore("BeverageStore", {
   state: () => ({
-    temps: tempretures,
+    temps: tempretures as string[],
     currentTemp: tempretures[0],
+
+    syrup: syrups as SyrupType[],
+    currentSyrup: syrups[0] as SyrupType,
+
+    cream: creamers as CreamerType[],
+    currentCreamer: creamers[0] as CreamerType,
+
+    base: bases as BaseBeverageType[],
+    currentBase: bases[0] as BaseBeverageType,
+
+    customBeverages: [] as BeverageType[],
+    currentBeverage: null as BeverageType | null,
+    nextId: 1
   }),
 
   actions: {
-    makeBeverage() {},
-    showBeverage() {},
+    makeBeverage(name: string) {
+      const trimName = name.trim();
+      if(!trimName) return;
+
+      const newBev: BeverageType = {
+        id: this.nextId.toString(),
+        name: trimName,
+        temp: this.currentTemp,
+        base: this.currentBase,
+        syrup: this.currentSyrup,
+        creamer: this.currentCreamer,
+      }
+      this.customBeverages.push(newBev);
+      this.currentBeverage = newBev;
+
+      this.nextId++;
+    },
+
+    showBeverage(id: string) {
+      const beverage = this.customBeverages.find((bev) => bev.id === id);
+      if(!beverage) return;
+
+      this.currentTemp = beverage.temp;
+      this.currentBase = beverage.base;
+      this.currentSyrup = beverage.syrup;
+      this.currentCreamer = beverage.creamer;
+    },
   },
+
   persist: true,
 });
